a678f04f192af5e8f28f9de9668373cc48039a43
William M. Keil <wkeil@infusion.com>
2015-11-24 11:11:39 -0500

    word tagger works now

--- word_tagger.py
+++ word_tagger.py
@@ -4,11 +4,18 @@ import nltk
 def annotator(text):
 	processedText = text.split()
 	annotatedText = []
+	"""annotator needs to tag the punc marks seperate from the words"""
+
 	"ask the user what p.o.s a word is, for every word in the text"
-		for word in processedText:
-			word_pos = input("Insert the POS of the word" + word + ": ")
-				if word:
-					annotatedText.append(word + "/" + word_pos)
+	for word in processedText:
+		word_pos = raw_input("Insert the POS of the word " + word + ": ")
+		if word:
+			annotatedText.append(word + "/" + word_pos)
+
+	annotatedText = str(annotatedText)
+	annotatedText = annotatedText.lower()
+	#this needs to be joined before list conversion #annotatedText = list(annotatedText)
+	return annotatedText
 
 	"take the input from the user and add a '/' between the pos and the word"
 
