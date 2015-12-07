5291898c102a3189e39d1ba83f4f536d796dd822
Michael Keil <w.michaelkeil@gmail.com>
2015-12-07 17:50:06 -0500

    adding local

--- word_tagger.py
+++ word_tagger.py
@@ -2,7 +2,7 @@
 import nltk
 
 def annotator(text):
-	processedText = text.split()
+	#processedText = text.split()
 	annotatedText = []
 	"""annotator needs to tag the punc marks seperate from the words"""
 
@@ -22,5 +22,21 @@ def annotator(text):
 	"""punctuation may be a problem, how to deal with seperating words from punctuation
 	and tag them as a punc mark"""
 
+
+def a(text):
+	#processedText = text.split()
+	annotatedText = []
+	"""annotator needs to tag the punc marks seperate from the words"""
+
+	"ask the user what p.o.s a word is, for every word in the text"
+	for word in text:
+		word_pos = raw_input("Insert the POS of the word " + word + ": ")
+		if word:
+			annotatedText.append(word + "/" + word_pos)
+
+	annotatedText = str(annotatedText)
+	annotatedText = annotatedText.lower()
+	#this needs to be joined before list conversion #annotatedText = list(annotatedText)
+	return annotatedText
 
 
