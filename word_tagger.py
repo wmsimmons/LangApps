c753ff2fc42fd775fc414bc9c17459e8f45a4d93
William M. Keil <wkeil@infusion.com>
2015-12-07 16:33:48 -0500

    word tagger works, added corpus information for tagger training

--- word_tagger.py
+++ word_tagger.py
@@ -1,5 +1,14 @@
 "this is to tag words"
 import nltk
+import sys
+
+def save():
+	#takes the result from annotator and adds it to a file
+	nameOfFile = raw_input("Which file is this to be placed in? ")
+
+	filename  = open(nameOfFile + '.txt','w')
+	sys.stdout = filename
+	#do work
 
 def annotator(text):
 	processedText = text.split()
@@ -11,16 +20,22 @@ def annotator(text):
 		word_pos = raw_input("Insert the POS of the word " + word + ": ")
 		if word:
 			annotatedText.append(word + "/" + word_pos)
-
+		
 	annotatedText = str(annotatedText)
-	annotatedText = annotatedText.lower()
+	annotatedText = annotatedText.lower().encode('utf-8')
 	#this needs to be joined before list conversion #annotatedText = list(annotatedText)
+
+	nameOfFile = raw_input("Which file is this to be placed in? ")
+
+	filename  = open(nameOfFile + '.txt','w')
+	sys.stdout = filename
+	sys.stdout annotatedText
+
 	return annotatedText
 
 	"take the input from the user and add a '/' between the pos and the word"
 
 	"""punctuation may be a problem, how to deal with seperating words from punctuation
 	and tag them as a punc mark"""
-
 
 
