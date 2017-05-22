3d4e47b49547b0741820f299b2ffdef622c96ee9
Michael O <Michael O>
2017-05-21 21:25:56 -0400

    changed syntax, added contact form

--- sanna/sannadictsite/main.py
+++ sanna/sannadictsite/main.py
@@ -1,17 +1,30 @@
 import os
-from flask import Flask
+from flask import Flask, flash
 from flask_bootstrap import Bootstrap
-from flask import render_template, request
+from flask import render_template, request, url_for
 from flask_pymongo import PyMongo
+from wtforms.validators import InputRequired
+from form import ContactForm
+
+from flask.ext.mail import Message, Mail
 
 #command to run app locally C:\Python34 .\python.exe C:\Users\langu\Desktop\qafasTaMalti\sanna\sannadictsite\main.py
 
 app = Flask(__name__)
 Bootstrap(app)
+mail = Mail()
 
+app.config['SECRET_KEY'] = '3bjd&hdj3%7@hdmSAN&**NA&**DICT&**%$324d'
 app.config['MONGO_DBNAME'] = 'sanna'
 app.config['MONGO_URI'] = 'mongodb://sanna:sanna@ds131109.mlab.com:31109/sanna'
 
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = 'user'
+app.config["MAIL_PASSWORD"] = 'password'
+
+mail.init_app(app)
 mongo = PyMongo(app)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -34,10 +47,25 @@ def resultDisplay():
 def aboutProj():
 	return render_template('aboutProject.html')
 
-@app.route('/contactus')
-def contactUs():
-	return render_template('contactUs.html')
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if request.method == 'POST':
+        if form.validate() == False:
+            flash('All fields are required.')
+            return render_template('contact.html', form=form)
+        else:
+            msg = Message(form.subject.data, sender='contact@example.com', recipients=['your_email@example.com'])
+            msg.body = """
+            From: {} <{}>
+            {}
+            """.format(form.name.data, form.email.data, form.message.data)
+            mail.send(msg)
 
+            return 'Form posted.'
+
+    elif request.method == 'GET':
+        return render_template('contact.html', form=form)
 
 """MUST be at end of program"""
 if __name__ == '__main__':
