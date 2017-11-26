dcdb49b763b8a0f40edd088aaa52f38534b98975
wmsimmons <w.michaelkeil@gmail.com>
2017-11-26 11:42:18 -0500

    Delete views.py

--- langShack/xacatlahtolli/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-
-from .models import Projects
-
-# Create your views here.
-def projects(request):
-	projects = Projects.objects.all()
-	return render(request, 'projects.html', {'projects': projects})
