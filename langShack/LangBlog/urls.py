5ec17479fa280d2af158730312de4b82d10320f1
wmsimmons <w.michaelkeil@gmail.com>
2017-12-27 22:53:19 -0500

    Delete urls.py

--- langShack/LangBlog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-from django.views.generic import ListView, DetailView
-from .models import Post
-
-urlpatterns = [ 
-	url(r'^$', ListView.as_view(
-	                    queryset=Post.objects.all().order_by("-created_at")[:25],
-	                    template_name="langblog.html")),
-]
