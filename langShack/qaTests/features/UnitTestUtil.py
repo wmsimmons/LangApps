01019d507f224b3ad9c20c3f7b0f676208104226
Michael Keil <mkeil@softpro.corp>
2018-08-06 17:37:28 -0400

    new tests for site

--- langShack/qaTests/features/UnitTestUtil.py
+++ langShack/qaTests/features/UnitTestUtil.py
@@ -2,70 +2,75 @@ from selenium.webdriver.common.keys import Keys
 import unittest
 from selenium.webdriver.common.action_chains import ActionChains
 
-def openPage(CurrentDriver, page, titleToVerify, assertion=None):
-    CurrentDriver.get(page)
-    title = CurrentDriver.title
-    
-    if assertion is True:
-        assert titleToVerify in title
+class Page:
+    def __init__(self, url):
+        self.url = url
+        
+    def openPage(self, currentDriver, titleToVerify, assertion=None):
+        print(self.url)
+        currentDriver.get(self.url)
+        title = currentDriver.title
+        
+        if assertion is True:
+            assert titleToVerify in title
 
-def close(CurrentDriver):
-    CurrentDriver.close()
+    def close(self, currentDriver):
+        currentDriver.close()
 
-def writeElement(CurrentDriver, selectorType, elem, valueToWrite, assertion=None):
-    # use id, xp, cs, 
-    if selectorType == 'id':
-        newElem = CurrentDriver.find_element_by_id(elem)
+    def writeElement(self, currentDriver, selectorType, elem, valueToWrite, assertion=None):
+        # use id, xp, cs, 
+        if selectorType == 'id':
+            newElem = currentDriver.find_element_by_id(elem)
 
-    if selectorType == 'xp':
-        newElem = CurrentDriver.find_element_by_xpath(elem)
+        if selectorType == 'xp':
+            newElem = currentDriver.find_element_by_xpath(elem)
 
-    if selectorType == 'cs':
-        newElem = CurrentDriver.find_element_by_css_selector(elem)
+        if selectorType == 'cs':
+            newElem = currentDriver.find_element_by_css_selector(elem)
 
-    if selectorType == 'tag':
-        newElem = CurrentDriver.find_element_by_tag_name(elem)
+        if selectorType == 'tag':
+            newElem = currentDriver.find_element_by_tag_name(elem)
 
-    if selectorType == 'name':
-        newElem = CurrentDriver.find_element_by_name(elem)
+        if selectorType == 'name':
+            newElem = currentDriver.find_element_by_name(elem)
 
-    # send chars to the element defined
-    newElem.sendKeys(valueToWrite)
+        # send chars to the element defined
+        newElem.sendKeys(valueToWrite)
 
-    # only asserts if user turns assert on
-    if assertion is True:
-        assert valueToWrite in newElem
-        if False:
-            print("The value was not the same as what was written" + newElem)
+        # only asserts if user turns assert on
+        if assertion is True:
+            assert valueToWrite in newElem
+            if False:
+                print("The value was not the same as what was written" + newElem)
 
 
-def clickElement(CurrentDriver, selectorType, elem):
-    # use id, xp, cs, 
-    if selectorType == 'id':
-        newElem = CurrentDriver.find_element_by_id(elem)
+    def clickElement(self, currentDriver, selectorType, elem):
+        # use id, xp, cs, 
+        if selectorType == 'id':
+            newElem = currentDriver.find_element_by_id(elem)
 
-    if selectorType == 'xp':
-        newElem = CurrentDriver.find_element_by_xpath(elem)
+        if selectorType == 'xp':
+            newElem = currentDriver.find_element_by_xpath(elem)
 
-    if selectorType == 'cs':
-        newElem = CurrentDriver.find_element_by_css_selector(elem)
+        if selectorType == 'cs':
+            newElem = currentDriver.find_element_by_css_selector(elem)
 
-    if selectorType == 'tag':
-        newElem = CurrentDriver.find_element_by_tag_name(elem)
+        if selectorType == 'tag':
+            newElem = currentDriver.find_element_by_tag_name(elem)
 
-    if selectorType == 'name':
-        newElem = CurrentDriver.find_element_by_name(elem)
+        if selectorType == 'name':
+            newElem = currentDriver.find_element_by_name(elem)
 
-    newElem.click()
+        newElem.click()
 
-    try:
-        chains = ActionChains(CurrentDriver)
-        chains.move_to_element(newElem)
-        chains.click()
-        chains.perform()
-    except ValueError:
-        print("Could not find element to click: " + newElem)
+        try:
+            chains = ActionChains(currentDriver)
+            chains.move_to_element(newElem)
+            chains.click()
+            chains.perform()
+        except ValueError:
+            print("Could not find element to click: " + newElem)
 
-# def clickMultipleElements(CurrentDriver, selectorType, elemList):
+    # def clickMultipleElements(self, CurrentDriver, selectorType, elemList):
 
-# def writeMultipleElement(CurrentDriver, selectorType, elemList, listOfValues, assertion=None):
+    # def writeMultipleElement(self, CurrentDriver, selectorType, elemList, listOfValues, assertion=None):
--- langShack/qaTests/features/langShackSmokeTest.py
+++ langShack/qaTests/features/langShackSmokeTest.py
@@ -1,4 +1,4 @@
-import UnitTestUtil
+from UnitTestUtil import Page
 from selenium import webdriver
 
 CurrentDriver = webdriver.Chrome()
@@ -11,14 +11,20 @@ addComment = "body > h2 > a"
 author = "id_author"
 textboxy = "id_text"
 
-# navigates to the langShack homepage
-UnitTestUtil.openPage(CurrentDriver, page, langShack_title, assertion=True)
+def testTheBlogTest():
+    ls_page = Page("http://langshack.org")
+    print(ls_page)
 
-UnitTestUtil.clickElement(CurrentDriver, "id", langBlogPage)
-UnitTestUtil.clickElement(CurrentDriver, "cs", firstPostLink)
-UnitTestUtil.clickElement(CurrentDriver, "cs", addComment)
+    # navigates to the langShack homepage
+    ls_page.openPage(CurrentDriver, langShack_title, assertion=True)
 
-UnitTestUtil.writeElement(CurrentDriver, "name", author, "haha")
-UnitTestUtil.writeElement(CurrentDriver, "name", textboxy, "test comment")
+    ls_page.clickElement(CurrentDriver, "id", langBlogPage)
+    ls_page.clickElement(CurrentDriver, "cs", firstPostLink)
+    ls_page.clickElement(CurrentDriver, "cs", addComment)
 
-UnitTestUtil.close(CurrentDriver)
+    ls_page.writeElement(CurrentDriver, "name", author, "haha")
+    ls_page.writeElement(CurrentDriver, "name", textboxy, "test comment")
+
+    ls_page.close(CurrentDriver)
+    
+testTheBlogTest()
